37d8eb2b11f37362ba5ea7713b63a7c21de8eea5
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-07 23:55:42 +0900

    WIP : ignore compiled files

---
+++ flip/src/script/script.js
@@ -0,0 +1,28 @@
+const gemstones = {
+  garnet: 'january',
+  amethyst: 'february',
+  aquamarine: 'march',
+  diamond: 'april',
+  emerald: 'may',
+  alexandrite: 'june',
+  ruby: 'july',
+  peridot: 'august',
+  sapphire: 'september',
+  tourmaline: 'october',
+  topaz: 'november',
+  turquoise: 'december'
+};
+const colors = {
+  garnet: '#8B0206',
+  amethyst: '#694F91',
+  aquamarine: '#7FC7F3',
+  diamond: '#F1F9FF',
+  emerald: '#04A24F',
+  alexandrite: '#5D9094',
+  ruby: '#F70004',
+  peridot: '#B1E347',
+  sapphire: '#2345B9',
+  tourmaline: '#FC5191',
+  topaz: '#F98603',
+  turquoise: '#4DF5FF'
+}
