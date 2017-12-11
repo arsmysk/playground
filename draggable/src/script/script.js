d72bc264f30c024f31f4ab3f084b32ddfaba013d
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-11 23:35:34 +0900

    install draggable

---
+++ draggable/src/script/script.js
@@ -0,0 +1,9 @@
+import { Draggable } from '@shopify/draggable';
+
+const draggable = new Draggable(document.querySelectorAll('ul'), {
+  draggable: 'li'
+})
+
+draggable.on('drag:start', () => console.log('drag:start'))
+draggable.on('drag:move', () => console.log('drag:move'))
+draggable.on('drag:stop', () => console.log('drag:stop'))
