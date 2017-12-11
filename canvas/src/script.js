0396df5816cd98eefedd844151003d1c5051b499
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-11 22:27:02 +0900

    WIP: rewrite  to class component from simple component

--- canvas/src/script.js
+++ canvas/src/script.js
@@ -1,34 +1,48 @@
+
+
+class RandomBall {
+  constructor(x, y, xSpeed, ySpeed) {
+    this.x = x
+    this.y = y
+    this.xSpeed = xSpeed
+    this.ySpeed = ySpeed
+  }
+
+  draw() {
+    ctx.clearRect(0, 0, elm.width, elm.height)
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2)
+    ctx.globalAlpha = 0.5
+    ctx.fillStyle = "#77B29C"
+    ctx.fill()
+    ctx.closePath()
+  }
+
+  updatePosition() {
+    /** speedずつ移動させる */
+    this.x += this.xSpeed
+    this.y += this.ySpeed
+
+    /** 範囲外になると方向を反転する */
+    if (this.x < 0 || this.x > elm.width) this.xSpeed = this.xSpeed * -1
+    if (this.y < 0 || this.y > elm.height) this.ySpeed = this.ySpeed * -1
+  }
+
+  render() {
+    this.draw()
+    this.updatePosition()
+
+    /** 再帰的に実行 */
+    requestAnimationFrame(this.render())
+  }
+}
+
 let elm = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
-let x = y = 100
-let xSpeed = ySpeed = 5
 
 elm.width = window.innerWidth
 elm.height = window.innerHeight
 
-const ball = () => {
-  ctx.clearRect(0, 0, elm.width, elm.height)
-  ctx.beginPath()
-  ctx.arc(x, y, 50, 0, Math.PI * 2)
-  ctx.globalAlpha = 0.5
-  ctx.fillStyle = "#77B29C"
-  ctx.fill()
-  ctx.closePath()
-}
+const randomBall = new RandomBall(100, 100, 5, 5)
 
-const draw = () => {
-  ball()
-  /** speedずつ移動させる */
-  x += xSpeed
-  y += ySpeed
-
-  /** 範囲外になると方向を反転する */
-  if (x < 0 || x > elm.width) xSpeed = xSpeed * -1
-  if (y < 0 || y > elm.height) ySpeed = ySpeed * -1
-
-  /** 再帰的に実行 */
-  requestAnimationFrame(draw)
-
-}
-
-draw()
+randomBall.render()
