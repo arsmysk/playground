99a6bc0e516c3a0a63911df56bebc3a8969002b0
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2018-02-01 16:02:42 +0900

    rewrite using ejs

--- node-basic/server.js
+++ node-basic/server.js
@@ -1,21 +1,21 @@
 const http = require('http')
 const fs = require('fs')
+const ejs = require('ejs')
 const settings = require('./settings')
 
 let server = http.createServer()
-let msg;
-
+let template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8')
+let n = 0
 server.on('request', (req, res) => {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' })
-      res.write("not found!")
-      return res.end()
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write(data)
-    res.end()
+  n++
+  let data = ejs.render(template, {
+    title: "hello",
+    content: "<strong>World!</strong>",
+    n: n
   })
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(data)
+  res.end()
 })
 server.listen(settings.port, settings.host)
 console.log("server listening...")
