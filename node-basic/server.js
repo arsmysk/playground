def0aa6fb42e40d4c42c9851c6b583164ecd3d23
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2018-02-01 14:56:00 +0900

    devide message to switch URL

--- node-basic/server.js
+++ node-basic/server.js
@@ -2,9 +2,22 @@ const http = require('http')
 const settings = require('./settings')
 
 let server = http.createServer()
+let msg;
+
 server.on('request', (req, res) => {
+  switch (req.url) {
+    case '/about':
+      msg = "about this page"
+      break
+    case '/profile':
+      msg = "about me"
+      break
+    default:
+      msg = 'wrong page'
+      break
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('hello world')
+  res.write(msg)
   res.end()
 })
 server.listen(settings.port, settings.host)
