f4fdb637283430c56495e3f60553e69f689c60e8
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2018-02-01 15:06:29 +0900

    load html

--- node-basic/server.js
+++ node-basic/server.js
@@ -1,24 +1,21 @@
 const http = require('http')
+const fs = require('fs')
 const settings = require('./settings')
 
 let server = http.createServer()
 let msg;
 
 server.on('request', (req, res) => {
-  switch (req.url) {
-    case '/about':
-      msg = "about this page"
-      break
-    case '/profile':
-      msg = "about me"
-      break
-    default:
-      msg = 'wrong page'
-      break
-  }
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write(msg)
-  res.end()
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.write("not found!")
+      return res.end()
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(data)
+    res.end()
+  })
 })
 server.listen(settings.port, settings.host)
 console.log("server listening...")
