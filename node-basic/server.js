d0b5c1ee1420ef160b945c5ca500e8ac4f02c365
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2018-02-01 14:30:57 +0900

    Rewrite with variables using extract file

--- node-basic/server.js
+++ node-basic/server.js
@@ -1,9 +1,11 @@
-let http = require('http')
+const http = require('http')
+const settings = require('./settings')
+
 let server = http.createServer()
 server.on('request', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.write('hello world')
   res.end()
 })
-server.listen(1337, '10.0.1.7')
+server.listen(settings.port, settings.host)
 console.log("server listening...")
